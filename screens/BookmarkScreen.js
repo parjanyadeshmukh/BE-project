ee9a7322b980f01a7bd584b1fbf989e1ad26ee94
parjanyadeshmukh <parjanya.deshmukh.pd@gmail.com>
2020-12-09 01:23:46 +0530

    Styling of meetingscheduled

--- screens/BookmarkScreen.js
+++ screens/BookmarkScreen.js
@@ -8,8 +8,8 @@ import 'react-native-gesture-handler';
 const BookmarkScreen=()=>{ 
     return(
       <View style={styles.container}>
-        <Text>
-        Meeting to be Schedule
+        <Text style={styles.text1}>
+        List Of Meetings Scheduled
         </Text>
       </View>
     )
@@ -20,8 +20,19 @@ const BookmarkScreen=()=>{
 const styles=StyleSheet.create({
     container:{
         flex:1,
-        alignItems:'center'
+        alignItems:'center',
     },
+    text1:{
+      fontSize:35,
+      fontWeight:'bold',
+      color:'#455a64',
+      textAlign:'center',
+      padding:10,
+      fontFamily:"sans-serif",
+      borderColor:'#455a64',
+      borderWidth:7,
+      
+    }
 })
   
  
